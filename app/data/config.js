72e5e50b4c559bbe2a908bb7d93080450ac79980
fominykhartur <fominykhartur+GitHub@gmail.com>
2021-11-19 18:29:27 +0300

    Добавлена настройка окружения через env файл Изменен gitignore

--- app/data/config.js
+++ app/data/config.js
@@ -1,11 +1,19 @@
 const mysql = require('mysql2');
 
+// const config = {
+//     host: 'localhost',
+//     user: 'root',
+//     password: 'root',
+//     database: 'library-db',
+// };
+require('dotenv').config();
+
 const config = {
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'library-db',
-};
+    host: process.env.host,
+    user: process.env.user,
+    password: process.env.password,
+    database: process.env.database,
+}
 
 const pool = mysql.createPool(config);
 
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const routes = require('./app/routes/routes');
+require('dotenv').config();
 
 
 const app = express();
